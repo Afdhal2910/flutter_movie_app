33a5b2145c28ed84825c207f6659afc237ac2c65
Afdhaluddin <afdhal2910@gmail.com>
2024-11-20 13:05:40 +0800

    initial setup

---
+++ lib/app_constants.dart
@@ -0,0 +1,8 @@
+class AppConstants {
+  static const String baseUrl = "https://api.themoviedb.org/3";
+  static const String apiKey = "35bc820e9890afeca993ea0543e2d8ee";
+  static const String imageUrlw500 = "https://image.tmdb.org/t/p/w342";
+  static const String imageUrlOriginal = "https://image.tmdb.org/t/p/original";
+
+
+}
